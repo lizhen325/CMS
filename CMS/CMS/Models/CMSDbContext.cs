d14d5126664bdb5aef314dd6c07244fdace5b043
lizhen325 <zhenli336@gmail.com>
2016-04-13 02:46:01 +1200

    Project Created

---
+++ CMS/CMS/Models/CMSDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CMS.Models
+{
+    public class CMSDbContext : IdentityDbContext<ApplicationUser>, IDisposable
+    {
+        public CMSDbContext() : base("CMSDb", throwIfV1Schema: false) { }
+
+        public static CMSDbContext Create()
+        {
+            return new CMSDbContext();
+        }
+    }
+}
---
+++ CMS/CMS/Scripts/aa.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="angular-min.js" />
+angular.module('myApp', [])
+                    .controller('myController', function ($scope) {
+                        $scope.name = "lizhen";
+                    });
