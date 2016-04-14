5caa3e12bddbbb646542c315485c6f90c7ac5dcf
lizhen325 <zhenli336@gmail.com>
2016-04-15 03:27:52 +1200

    Completed Booking View: Date Picker using ui bootstrap

---
+++ CMS/CMS/Scripts/Views/Booking.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../angular-min.js" />
+angular.module('BookingModule', ['ui.bootstrap']).controller('DatepickerController', function ($scope) {
+    $scope.today = function () {
+        $scope.date = new Date();
+    };
+    $scope.today();
+
+    // Disable weekend selection
+    function disabled(data) {
+        var date = data.date,
+          mode = data.mode;
+        return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+    }
+
+    $scope.openDate = function () {
+        $scope.popup1.opened = true;
+    };
+
+    $scope.setDate = function (year, month, day) {
+        $scope.date = new Date(year, month, day);
+    };
+
+    $scope.popup1 = {
+        opened: false
+    }; 
+});
